c6adc46c378f2a04903260e2f4633c0b3a2569fb
lanzdelrosario <46342372+lanzdelrosario@users.noreply.github.com>
2019-01-03 17:37:59 +0800

    Add files via upload

---
+++ delrosario 1..cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	
+//1.	
+	char a;
+	
+	cout <<"enter a char: ";
+	cin >> a;
+	
+	for (int x= 0; x <= 9; x++){
+		cout << a;
+	}
+	return 0;
+}
+
+
---
+++ delrosario 2..cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char asc;
+	
+	cout << "Enter a character: ";
+	cin >> asc;
+	cout << "ASCII value of the character " << asc << " is " << int(asc);
+	return 0;
+	}
